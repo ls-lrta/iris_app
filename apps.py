544779ea4faea1d22a6681b495999f0faa40c05c
mkk32 <at1.mkk@gmail.com>
2024-03-03 21:47:56 +0800

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,13 +1,14 @@
 from sklearn.datasets import load_iris
 import pandas as pd
 import streamlit as st 
-import joblib
+import pickle
 import numpy as np
 
 species = ['setosa', 'versicolor', 'virginica']
 image = ['setosa.jpg', 'versicolor.jpg', 'virginica.jpg']
-with open('irismodel.joblib', 'rb') as f:
-    model = joblib.load(f)
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
 
 def main():
 
@@ -19,9 +20,9 @@ def main():
     petal_width = st.sidebar.slider("petal width (cm)",0.1,2.5,0.2)
 
     # Getting Prediction from model
-    inp = np.array([[sepal_length,sepal_width,petal_length,petal_width]])
-    #inp = np.expand_dims(inp,axis=0)
-    prediction = model.predict(inp)
+    inp = np.array([sepal_length,sepal_width,petal_length,petal_width])
+    inp = np.expand_dims(inp,axis=0)
+    prediction = model.predict_proba(inp)
 
     # Main page
     st.title("Iris Flower Classification")
@@ -34,12 +35,13 @@ def main():
         Following is the probability of each class
         ''')
         
-        #df = pd.DataFrame(prediction, index = ['result'], columns=species)
-        #st.dataframe(df)
+        df = pd.DataFrame(prediction, index = ['result'], columns=species)
+        st.dataframe(df)
         result = species[np.argmax(prediction)]
         st.write("**This flower belongs to " + result + " class**")
         st.image(image[np.argmax(prediction)])
 
- 
+    
+
 if __name__ == "__main__":
     main()
